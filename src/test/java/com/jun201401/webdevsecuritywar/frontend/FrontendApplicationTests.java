0ee62763879095a5017d01fb027e36ed461de860
jun201401 <jun.ti.gear@gmail.com>
2023-07-05 01:29:22 +0900

    :tada: Initial commit

---
+++ src/test/java/com/jun201401/webdevsecuritywar/frontend/FrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jun201401.webdevsecuritywar.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
