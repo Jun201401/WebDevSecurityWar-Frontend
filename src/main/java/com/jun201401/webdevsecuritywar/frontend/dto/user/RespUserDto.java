0dda230669fb35d8dc4a7d9211ca444d20358956
jun201401 <jun.ti.gear@gmail.com>
2023-07-05 16:08:30 +0900

    :sparkles: sign up 일부 구현

---
+++ src/main/java/com/jun201401/webdevsecuritywar/frontend/dto/user/RespUserDto.java
@@ -0,0 +1,12 @@
+package com.jun201401.webdevsecuritywar.frontend.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RespUserDto {
+    private String id;
+}
